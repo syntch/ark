7ae2797e6761f1fedda38ada03b33e736d14ad25
syntch <justin@gotboost.net>
2025-06-25 15:33:01 -0400

    Create Start_Server.bat

---
+++ Start_Server.bat
@@ -0,0 +1,3 @@
+@echo off
+start ShooterGame\Binaries\Win64\ArkAscendedServer.exe Ragnarok_WP?SessionName="Crossbar"?ServerPassword=foo?ServerAdminPassword=bar?Port=7777?QueryPort=27015 -NoBattlEye -log -crossplay
+exit
